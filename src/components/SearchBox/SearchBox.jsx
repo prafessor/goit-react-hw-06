5b7326e1690dd8a499544bd1abc0935786871cbc
prafessor <olegmilyo@gmail.com>
2025-04-10 07:37:47 +0300

    fix params for SearchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { changeFilter } from "../../redux/filtersSlice";
 import css from "./SearchBox.module.css";
 
-export default function SearchBox({ value, onChange }) {
+export default function SearchBox() {
   const dispatch = useDispatch();
   const filterValue = useSelector((state) => state.filters.name);
 
